79d7e9359b793dff573f36f43edcbbfb6a9ab08b
Elzilane Barreto <elzilanebarreto@gmail.com>
2026-01-15 18:03:41 -0400

    fix: collab phrase

--- src/components/contatos/Contatos.tsx
+++ src/components/contatos/Contatos.tsx
@@ -6,7 +6,7 @@ const Contatos = () => {
 
       <h3 className="text-center text-(--quaternaria) text-4xl font-bold m-3">Vamos criar algo incrível juntos?</h3>
 
-      <p className="text-center text-(--branco) p-5">Sinta-se à vontade para me chamar para um café virtual, tirar dúvidas sobre meu trabalho ou discutir novas oportunidades no mercado de tecnologia.</p>
+      <p className="text-center text-(--branco) p-5">Estou disponível para novos desafios e projetos inovadores. Se você tem uma ideia ou uma proposta de colaboração, adoraria ouvir e ajudar a torná-la real.</p>
 
       <div className="flex justify-around gap-5 p-4">
         <a href="mailto:elzilanecardoso24@gmail.com" className="text-2xl" target="_blank">
